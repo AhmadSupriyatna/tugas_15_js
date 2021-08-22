96eee983dd6ed3911cfe1ca0f794c3cc68475338
AhmadSupriyatna <ahmadsupriyatna1@gmail.com>
2021-08-22 21:44:21 +0700

    Tugas 15 Javascript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas15.js
@@ -0,0 +1,7 @@
+var ab = 8;
+var bc = 6;
+
+var ac = Math.sqrt(Math.pow(ab,2)+Math.pow(bc,2))
+console.log("Nilai AB : ", ab);
+console.log("NIlai BC : ", bc);
+console.log("Panjang sisi nilai AC pada segitiga siku-siku adalah : ",ac);
